1a1b210118b956e66a7b9569f35c5d7fbee6f871
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-16 10:29:39 -0300

    Parâmetros por URL

--- secao-6-express/index.js
+++ secao-6-express/index.js
@@ -6,19 +6,27 @@ const path = require("path")
 
 const basePath = path.join(__dirname, 'templates')
 
-const checkAuth = function (req, res, next) {
-  req.authStatus = true
+// const checkAuth = function (req, res, next) {
+//   req.authStatus = true
 
-  if (req.authStatus) {
-    console.log('Esta logado, pode continuar');
-    next()
-  } else {
-    console.log('Não está logado, faça login para continuar');
-    next()
-  }
-}
+//   if (req.authStatus) {
+//     console.log('Esta logado, pode continuar');
+//     next()
+//   } else {
+//     console.log('Não está logado, faça login para continuar');
+//     next()
+//   }
+// }
 
-app.use(checkAuth)
+//app.use(checkAuth)
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id
+
+  console.log(`Estamos buscando o usuário: ${id}`);
+
+  res.sendFile(`${basePath}/users.html`)
+})
 
 app.get('/', (req, res) => {
   res.sendFile(`${basePath}/index.html`)
