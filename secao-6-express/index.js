9f017258a792c351e70b563106baeb194b77937b
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-16 10:05:31 -0300

    Middleware verificar se usuário está logado

--- secao-6-express/index.js
+++ secao-6-express/index.js
@@ -6,6 +6,20 @@ const path = require("path")
 
 const basePath = path.join(__dirname, 'templates')
 
+const checkAuth = function (req, res, next) {
+  req.authStatus = true
+
+  if (req.authStatus) {
+    console.log('Esta logado, pode continuar');
+    next()
+  } else {
+    console.log('Não está logado, faça login para continuar');
+    next()
+  }
+}
+
+app.use(checkAuth)
+
 app.get('/', (req, res) => {
   res.sendFile(`${basePath}/index.html`)
 })
